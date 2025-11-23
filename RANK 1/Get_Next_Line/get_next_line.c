ddc08abd0ce2126407180d7263262b346d9be150
Linded_ <91735533+Feimily@users.noreply.github.com>
2025-11-23 14:52:32 +0100

    SUbida

--- RANK 1/Get_Next_Line/get_next_line.c
+++ RANK 1/Get_Next_Line/get_next_line.c
@@ -6,71 +6,95 @@
 /*   By: vaguayo- <vaguayo-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 12:32:09 by vaguayo-          #+#    #+#             */
-/*   Updated: 2025/11/11 15:11:42 by vaguayo-         ###   ########.fr       */
+/*   Updated: 2025/11/22 21:12:58 by vaguayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
+# define BUFFER_SIZE 1
 #endif
 
 char *line_reader(char **storage, int fd)
 {
     char        *buffer;
-	char 		*temp;
     ssize_t     bytes;
     
-    buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
     if (!*storage)
-        *storage = ft_calloc(1,1);
-	if (!*storage || !buffer)
-		return (free(buffer),NULL);
+        *storage = ft_calloc(1, 1);
+    if (!*storage)
+        return (NULL);
+    buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+    if (!buffer)
+        return (NULL);
     bytes = 1;
-    while (1)
+    while (!ft_strchr(*storage, '\n') && (bytes = read(fd, buffer, BUFFER_SIZE)) > 0)
     {
-        bytes = read(fd, buffer, BUFFER_SIZE);
-        if (bytes < 0)
-            return (free(buffer), NULL);
-        if (bytes == 0)
-            break;
-		temp = *storage;
+        buffer[bytes] = '\0';
+        char *temp = *storage;
         *storage = ft_strjoin(temp, buffer);
-		free(temp);
+        free(temp);
         if (!*storage)
-			return (free(buffer), NULL);
-		if (ft_strchr(buffer, '\n'))
-			break ;
+            return (free(buffer), NULL);
     }
+    if (bytes < 0)
+        return (free(*storage), *storage = NULL, free(buffer), NULL);
     free(buffer);
+    if (**storage == '\0')
+        return (free(*storage), *storage = NULL, NULL);
     return (*storage);
+}
+
+char *line_separator(char **storage)
+{
+    char    *line;
+    char    *tempstorage;
+    size_t  i;
+    
+    if (!*storage) 
+        return (NULL);
+    i = 0;
+    while((*storage)[i] && (*storage)[i] != '\n') 
+        i++;
+    if ((*storage)[i] == '\n')
+    {
+        line = ft_substr(*storage, 0, i + 1);
+        tempstorage = ft_substr(*storage, i + 1, ft_strlen(*storage) - (i + 1));
+        if (!line || !tempstorage)
+        {
+            free(line);
+            free(tempstorage);
+            free(*storage);
+            *storage = NULL;
+            return (NULL);
+        }
+        free(*storage);
+        *storage = tempstorage;
+    }
+    else
+    {
+        line = ft_substr(*storage, 0, ft_strlen(*storage));
+        free(*storage);
+        *storage = NULL;
+    }
+    if (!line)
+        return (NULL);
+    return (line);
 }
 
 char *get_next_line(int fd)
 {
     static char *storage;
-    char *line;
-    char *tempstorage;
+	char *line;
+    
     size_t i;
-
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
     if (line_reader(&storage, fd) == NULL)
-        return(NULL);
-    i = 0;
-    while(storage[i] && storage[i] != '\n')
-        i++;
-    if (storage[i] == '\n')
-    {
-        line = ft_substr(storage, 0, i + 1);
-        tempstorage = ft_substr(storage, i + 1, ft_strlen(storage) - (i + 1));
-		free(storage);
-		storage = tempstorage;
-    }
-	else
-	{
-		line = ft_substr(storage, 0, ft_strlen(storage));
-        free(storage);
-        storage = NULL;
-	}
-    return(line);
+            return(NULL);
+    line = line_separator(&storage);
+	if (!line)
+		return (NULL);
+	return (line);
 }
