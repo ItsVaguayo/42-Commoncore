80bdecd592dd85d56113349f61c177827797c6c4
Linded_ <91735533+Feimily@users.noreply.github.com>
2025-12-16 18:39:21 +0100

    Base

---
+++ RANK 2/prueba.c
@@ -0,0 +1,34 @@
+# include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+typedef struct s_node {
+    int value;
+    struct s_node *next;
+} t_node;
+
+/*int main(void)
+{
+    t_node n1;
+    t_node n2;
+    t_node n3;
+
+    n1.value = 10;
+    n2.value = 20;
+    n3.value = 30;
+    n1.next = &n2;
+    n2.next = &n3;
+    n3.next = NULL;
+    
+    printf("%d\n", n1.value);
+    printf("%d\n", n1.next ->value);
+    printf("%d\n", n1.next ->next->value);
+}*/          
+t_node *create_node(int val)
+{
+    t_node *new = malloc(sizeof(t_node));
+
+    new.next = NULL;
+
+}
---
+++ RANK 2/pushswap.h
@@ -0,0 +1,6 @@
+#ifndef PUSHSWAP_H
+# define PUSHSWAP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
